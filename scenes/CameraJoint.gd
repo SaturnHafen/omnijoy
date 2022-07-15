3abb45eca473bad3f78f6436029e35250e5a2a16
Christian2 <manz@uni-potsdam.de>
2022-07-15 22:52:21 +0200

    Add test scenes

---
+++ scenes/CameraJoint.gd
@@ -0,0 +1,21 @@
+extends SpringArm
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+onready var camerarRefrence:Camera = $Camera
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	set_as_toplevel(true)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	print($Camera.translation)
+	#$Camera.translation.x = 10
+	#$Camera.translation.y = 10
+	#$Camera.translation.z = 10
