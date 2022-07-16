9cf634d9650d8f7d1ab6a6527436b7298f1f4350
Christian2 <manz@uni-potsdam.de>
2022-07-16 12:11:51 +0200

    Fix für Kamera und Bewegung

--- scripts/MovementController.gd
+++ scripts/MovementController.gd
@@ -2,7 +2,7 @@ extends RigidBody
 
 var speed:float = 20
 
-onready var _spring_arm: SpringArm = $SpringArm
+#onready var _spring_arm: SpringArm = $SpringArm
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -16,7 +16,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	_spring_arm.translation = translation
+	#_spring_arm.translation = translation
 	#print(translation)
 	
 	var forceToAdd:Vector3 = Vector3.ZERO
@@ -28,8 +28,11 @@ func _process(delta):
 	if Input.is_action_pressed("ui_right"):
 		forceToAdd += Vector3.RIGHT
 	if Input.is_action_pressed("ui_up"):
-		forceToAdd += Vector3.FORWARD
-		
+		forceToAdd += Vector3.FORWARD #Ist Vekot (0,0,-1)
+	
+	forceToAdd = forceToAdd.rotated(Vector3.UP, rotation.y)
+	forceToAdd = forceToAdd.normalized()
+	
 	if forceToAdd != Vector3.ZERO:
 		add_central_force(forceToAdd * speed )
 
