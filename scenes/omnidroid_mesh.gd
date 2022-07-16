4c40003a6b2aa702914111b2a65c9518866ac5ca
Marius Dörbandt <github.marius@doerbandt.de>
2022-07-16 11:13:04 +0200

    Move on signal

--- scenes/omnidroid_mesh.gd
+++ scenes/omnidroid_mesh.gd
@@ -5,4 +5,15 @@ func _ready():
 	$Skeleton/ik_front.start()
 	$Skeleton/ik_left.start()
 	$Skeleton/ik_right.start()
-	
+
+func _on_Player0_move(acceleration):
+	$front.translate(acceleration)
+
+func _on_Player1_move(acceleration):
+	$back.translate(acceleration)
+
+func _on_Player2_move(acceleration):
+	$left.translate(acceleration)
+
+func _on_Player3_move(acceleration):
+	$right.translate(acceleration)
