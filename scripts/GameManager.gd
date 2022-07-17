477b3891c117764f8e4fd2312ded8e64c24e6824
codingWhale13 <nils.kiele@gmail.com>
2022-07-17 13:15:03 +0200

    add timer bar

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -1,5 +1,12 @@
 extends Spatial
 
+const TIME_TO_KILL = 100
+
+# these values will be set after spawn
+var timer
+var timerProgressBar
+var timeLeftText
+
 var manager = preload("res://JoyCons/JoyConManager.gd").new()
 var scene = preload("res://scenes/CanyonLowPoly.tscn")
 var omnidroid = load("res://scenes/omnidroid.tscn")
@@ -10,17 +17,24 @@ var current_droid: Node
 func _ready():
 	if manager.init_devices() < 4:
 		assert(false, "There must be 4 JoyCons")
-	
 	initialize_main_scene()
 
+func _process(delta):
+	if is_instance_valid(timer):
+		timerProgressBar.value = timer.time_left
+		var styleBox = timerProgressBar.get("custom_styles/fg")
+		var r = 1 - timerProgressBar.value / timerProgressBar.max_value
+		var g = timerProgressBar.value / timerProgressBar.max_value
+		styleBox.bg_color = Color(r, g, 0)
+	if is_instance_valid(timeLeftText):
+		timeLeftText.text = 'TIME TO KILL: ' + str(int(timerProgressBar.value))
 
 func initialize_main_scene():
 	current_scene = scene.instance()
 	add_child(current_scene)
 	print(current_scene)
-	
 	spawn_droid()
-	
+
 func spawn_droid():
 	var spawn_point = current_scene.get_node("spawn").translation
 	
@@ -30,11 +44,15 @@ func spawn_droid():
 	droid.get_node("mesh").init(manager)
 	current_droid = droid
 	
+	timer = droid.get_node("Camera/playerInfo/Timer")
+	timerProgressBar = droid.get_node("Camera/playerInfo/TimerProgressBar")
+	timeLeftText = droid.get_node("Camera/playerInfo/TimeLeftText")
+	timer.wait_time = TIME_TO_KILL
+	timerProgressBar.min_value = 0
+	timerProgressBar.max_value = TIME_TO_KILL
+	timer.start()
+
 func game_over():
 	current_scene.queue_free()
 	current_droid.queue_free()
 	initialize_main_scene()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
